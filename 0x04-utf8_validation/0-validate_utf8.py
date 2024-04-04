4b3c6484b7f7606cffe84b256ece1d6b2e867c29
Norberts <mainanorbert@gmail.comm>
2024-04-04 15:49:08 +0300

    validating utf-8 data

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,7 +7,7 @@ def validUTF8(data):
     """function determines if a given data set
     represents a valid UTF-8 encoding"""
     for x in data:
-        x = x & 0xFF
+        # x = x & 0xFF
         # x = int(bin(x), 2)
         if is_valid_utf8_encoding(x) is False:
             return False
