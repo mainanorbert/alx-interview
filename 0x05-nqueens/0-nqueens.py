c5b114409a70cbaab57216dcedee773a3ba9d36a
Norberts <mainanorbert@gmail.comm>
2024-04-11 19:59:06 +0300

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,23 +1,67 @@
-#!/usr/bin/python3
-"""N queens puzzel"""
-
+#!/usr/bin/env python3
+""" N queens"""
 import sys
 
 
-def nqueen_puzzle():
-    """N queens"""
+def is_safe(board, row, col, N):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check upper diagonal on right side
+    for i, j in zip(range(row, -1, -1), range(col, N)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens(board, row, N, solutions):
+    if row == N:
+        # Convert board to list of queen positions and add to solutions
+        solution = []
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row][col] = 1
+            solve_nqueens(board, row + 1, N, solutions)
+            board[row][col] = 0
+
+
+def nqueens(N):
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solutions = []
+    solve_nqueens(board, 0, N, solutions)
+    return solutions
+
+
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: nqueens N")
+        print("Usage: ./0-nqueens.py N")
         sys.exit(1)
-    n_queen = sys.argv[1]
+
     try:
-        n_queen = int(n_queen)
+        N = int(sys.argv[1])
     except ValueError:
-        print('N must be a number')
+        print("N must be a number")
         sys.exit(1)
-    if n_queen < 4:
+
+    if N < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-
-nqueen_puzzle()
+    solutions = nqueens(N)
+    for solution in solutions:
+        print(solution)
