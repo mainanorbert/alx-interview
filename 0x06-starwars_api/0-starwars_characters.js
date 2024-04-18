44faf6022d24230282c434a89e0851e588e66624
Norberts <mainanorbert@gmail.comm>
2024-04-18 12:53:23 +0300

    star api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,50 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const getStarWarsCharacters = (movieId) => {
-  const url = `https://swapi.dev/api/films/${movieId}`;
-
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error(`Error retrieving movie data: ${error}`);
-      return;
-    }
-
-    if (response.statusCode === 200) {
-      const data = JSON.parse(body);
-      const characters = data.characters;
-
-      if (characters) {
-        characters.forEach((characterUrl) => {
-          request(characterUrl, (characterError, characterResponse, characterBody) => {
-            if (characterError) {
-              console.error(`Error retrieving character data: ${characterError}`);
-              return;
-            }
-
-            if (characterResponse.statusCode === 200) {
-              const characterData = JSON.parse(characterBody);
-              console.log(characterData.name);
-            } else {
-              console.error(`Error retrieving character data: ${characterResponse.statusCode}`);
-            }
-          });
-        });
-      } else {
-        console.log('No characters found for this movie.');
-      }
-    } else {
-      console.error(`Error retrieving movie data: ${response.statusCode}`);
-    }
-  });
-};
-
-// Get movie ID from command line argument (assuming script is run as node script.js movie_id)
 const movieId = process.argv[2];
 
-if (movieId) {
-  getStarWarsCharacters(parseInt(movieId));
-} else {
-  console.error('Please provide a movie ID as an argument.');
-}
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+request(url, (error, response, body) => {
+  if (error || response.statusCode !== 200) {
+    process.exit(1);
+  }
+  const charactersUrls = JSON.parse(body).characters;
+	console.log(charactersUrls)
+  charactersUrls.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        process.exit(1);
+      }
+      console.log(JSON.parse(body).name);
+    });
+  });
+});
