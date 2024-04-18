4ed6f94b5c235f33a9987b68d3353c03e97a65d9
Norberts <mainanorbert@gmail.comm>
2024-04-18 11:39:24 +0300

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,20 +5,16 @@ const movieId = process.argv[2];
 
 const url = `https://swapi.dev/api/films/${movieId}/`;
 request(url, (error, response, body) => {
-  if (error) {
+  if (error || response.statusCode !== 200) {
     process.exit(1);
   }
-  if (response.statusCode === 200) {
-    const charactersUrls = JSON.parse(body).characters;
-    charactersUrls.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          process.exit(1);
-        }
-        if (response.statusCode === 200) {
-          console.log(JSON.parse(body).name);
-        }
-      });
+  const charactersUrls = JSON.parse(body).characters;
+  charactersUrls.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        process.exit(1);
+      }
+      console.log(JSON.parse(body).name);
     });
-  }
+  });
 });
