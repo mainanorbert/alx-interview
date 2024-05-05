2fca665a708eb8645a4045cc9fed1038edfb7fef
Norberts <mainanorbert@gmail.comm>
2024-05-05 15:42:58 +0300

    minimum coins to make change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,14 +1,31 @@
-#!/usr/bin/python3
-"""module for Change comes from within"""
+#!/usr//bin/python3
+""" module for Change comes from within"""
 
 
 def makeChange(coins, total):
-    """function for change comes from within"""
-    if total < 0:
+    """making change"""
+    if total <= 0:
         return 0
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    coins = sorted(coins, reverse=True)
+    current_t = 0
+    no_of_coins = 0
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-    return dp[total] if dp[total] != float('inf') else -1
+        if coin > total:
+            continue
+        current_t += coin
+        no_of_coins += 1
+        if current_t == total:
+            return no_of_coins
+        if current_t > total:
+            current_t = current_t - coin
+            no_of_coins = no_of_coins - 1
+            continue
+        while current_t < total:
+            current_t += coin
+            if current_t == total:
+                return no_of_coins + 1
+            if current_t > total:
+                current_t = current_t - coin
+                break
+            no_of_coins += 1
+    return -1
