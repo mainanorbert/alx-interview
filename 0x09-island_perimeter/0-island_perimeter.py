f3d1ff83fe393ba567a59afea5e8e875fd40cbc1
Norberts <mainanorbert@gmail.comm>
2024-05-09 10:39:09 +0300

    perimeter of island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""Module that calculates perimeter of the island described in grid"""
+"""module that returns the perimeter of the island described in grid"""
 
 
 def island_perimeter(grid):
-    """function that  calculates perimeter of the island described in grid"""
+    """function that returns the perimeter of the island described in grid"""
     perimeter = 0
     for x in range(len(grid)):
         for y in range(len(grid[0])):
             if grid[x][y] == 1:
-                if x > 0 and y > 0:
-                    if grid[x - 1][y] == 0:
-                        perimeter += 1
-                    if grid[x + 1][y] == 0:
-                        perimeter += 1
-                    if grid[x][y-1] == 0:
-                        perimeter += 1
-                    if grid[x][y + 1] == 0:
-                        perimeter += 1
+                perimeter += 4
+                if x < len(grid) - 1:
+                    if grid[x + 1][y] == 1:
+                        perimeter = perimeter-2
+                if y < len(grid[0]) - 1:
+                    if grid[x][y + 1] == 1:
+                        perimeter = perimeter - 2
     return perimeter
